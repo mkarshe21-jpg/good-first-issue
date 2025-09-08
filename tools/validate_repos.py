4e7bb457f6360d48cdde3c8877ddf3a95a721a28
mkarshe21-jpg <mkarshe21@gmail.com>
2025-09-08 04:02:02 -0600

    Update validate_repos.py
    Signed-off-by: mkarshe21-jpg <mkarshe21@gmail.com>

--- tools/validate_repos.py
+++ tools/validate_repos.py
@@ -5,3 +5,12 @@ try:
     Import tomli as toml
 except Exception:
     toml = None
+
+import aiohttp
+from datetime import datetime, timedelta, timezone
+REPO_FILE = "data/repositories.toml"
+GITHUB_API = "https://api.github.com"
+LABEL= "good first issue"
+MIN_GFI = 3
+MIN_CONTRIB = 10
+ACTIVE_DAYS = 180
